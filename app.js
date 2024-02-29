0a9a2255dc15da6d7c46244adeebb4234ee8ad5b
boburbo <bobur20081608@gmail.com>
2024-03-01 01:05:16 +0500

    4.9vazifasi

---
+++ app.js
@@ -0,0 +1,58 @@
+// kop ishlatiladigon string metodlari
+let email = 'boburweb@gmail.com'
+
+//LastIndof() Usul lastIndexOf()satrda belgilangan qiymatning oxirgi paydo bo'lishi indeksini (pozitsiyasini) qaytaradi
+// let result = email.lastIndexOf('b') ohridagini olib beradi
+
+
+// //indexof
+// let result = email.indexOf('b')
+// console.log(result);
+
+//slice()
+// let result = email.slice(5,10)
+// console.log(result);
+
+//substr()  Usul substr()satrning bir qismini chiqaradi.
+
+// let result = email.substr(4, 10)   belgilanagan joyni kesip beradi!
+// console.log(result);
+
+//replace()Usul replace()qiymat yoki oddiy ifoda uchun satrni qidiradi.
+
+// let result = email.replace('b','l') bu almashtrib beradi
+// console.log(result);
+
+
+
+//charAt() Aytilgan buyruq boyicha olip beradi malumotni
+// let result = email.charAt(5)
+// console.log(result);
+
+// //toUUppercase()
+
+// let result = email.toUpperCase()
+// console.log(result);
+
+
+// const ism = prompt('Ismingizni kiriting')
+// const yosh = +prompt('Yoshingizni kiriting')
+// const result = `Qadirli ${ism}.Siz${2022 - yosh} yilda tugilgansiz.Va siz ${yosh * 12}
+// ou,${(yosh * 365) / 7} hafta,${yosh * 365} kun,${yosh * 365 * 24} soat,${yosh * 365*24* 60} daqiqa,${yosh * 365*24* 60 * 60} soniya yashagansiz.😉`
+// alert(result)
+// //*2) ismlar array ichidan prompdan soralyotkan
+// //ism bolsa unda ha bunday ism bor agar bolmasa bunday ism yoq deydi
+// const ismlar = ['Ahror','Sardor','Doniyor']
+
+
+// const ism = prompt('Ismingizni kiriting')
+// const yosh = +prompt('Yoshingizni kiriting')
+// const result = `Qadirli ${ism}.Siz${2022 - yosh} yilda tugilgansiz.Va siz ${yosh * 12}
+// ou,${(yosh * 365) / 7} hafta,${yosh * 365} kun,${yosh * 365 * 24} soat,${yosh * 365*24* 60} daqiqa,${yosh * 365*24* 60 * 60} soniya yashagansiz.😉`
+// alert(result)
+// //*2) ismlar array ichidan prompdan soralyotkan
+// //ism bolsa unda ha bunday ism bor agar bolmasa bunday ism yoq deydi
+// const ismlar = ['Ahror','Sardor','Doniyor']
+
+
+.
